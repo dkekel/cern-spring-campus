23445d3ee47795789212d12aac071d40b8c4e565
dkekel <dkekel@gmail.com>
2019-05-28 21:56:29 +0200

    Spring Boot Demo

---
+++ src/main/java/ch/meyrin/cern/springcampus/SpringCampusApplication.java
@@ -0,0 +1,13 @@
+package ch.meyrin.cern.springcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/meyrin/cern/springcampus/SpringCampusApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.meyrin.cern.springcampus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCampusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
