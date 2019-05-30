6ee422c62e74a4bbfda1081c5cca9015a2b12bdf
dkekel <dkekel@gmail.com>
2019-05-30 12:57:04 +0200

    Add themeleaf dependency, add mvc configuration, map index to root path

--- src/main/java/ch/meyrin/cern/springcampus/controller/BonjourController.java
+++ src/main/java/ch/meyrin/cern/springcampus/controller/BonjourController.java
@@ -2,14 +2,12 @@ package ch.meyrin.cern.springcampus.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class BonjourController {
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "index";
     }
 }
